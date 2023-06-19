21f97fd8884960e8483b239d12c6d5ce527df6dd
Guirazz <137099864+Guirazz@users.noreply.github.com>
2023-06-19 14:32:13 -0300

    Alteração opção de saída
    Inclusão da opção de saída de sistema

--- Starter_ebac1.c
+++ Starter_ebac1.c
@@ -128,6 +128,7 @@ int main()
 	  printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
 	  printf("\t3 - Deletar nomes\n\n"); 
+	  printf("\t4 - Sair do sistema\n\n");
 	  printf("Opcao:"); //fim do menu
 
       scanf("%d", &opcao);  //armazenando a escolha do usuário
@@ -149,6 +150,10 @@ int main()
     		consulta();
          	break;
      	
+     	case 4:
+     		printf("Obrigado por utilizar o sistema!\n");
+     		return 0;
+     		break;
      	     	
      	default:
      		printf("Essa opcao não está disponível!\n");
